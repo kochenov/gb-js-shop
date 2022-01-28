37c9be4d6ef53bb4f9a640935ce3124464d004b2
kochenov <kochenof@gmail.com>
2022-01-28 18:00:49 +0300

    Практическое задание №1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import getProductList from "./mock/data.js";
 import renderGoodsList from "./showcase.js";
+import "./assets/style.scss";
+
 
 const productList = getProductList(20);
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,49 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: {
-        main: path.resolve(__dirname, './src/index.js'),
-    },
+  mode: 'development',
+  entry: {
+    main: path.resolve(__dirname, './src/index.js'),
+  },
 
-    output: {
-      path: path.resolve(__dirname, './dist'),
-      filename: '[name].bundle.js',
-    },
-
-    plugins: [
-      new HtmlWebpackPlugin({
-          title: 'GeekBrains Shop JS2',
-          template: path.resolve(__dirname, './public/template.html'), // шаблон
-          filename: 'index.html', // название выходного файла
-      }),
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
+    clean: true, // для очистки папки dist при новом билде
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
     ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'GeekBrains Shop JS2',
+      template: path.resolve(__dirname, './public/template.html'), // шаблон
+      filename: 'index.html', // название выходного файла
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "style.css",
+      chunkFilename: "[id].css",
+    }),
+  ],
 }
