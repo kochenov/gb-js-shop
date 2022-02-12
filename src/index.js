78269b1ade1540302937402d5ad668c30619b8e1
kochenov <kochenof@gmail.com>
2022-02-12 18:29:58 +0300

    ДЗ-DELETE

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
-import getProductList from "./mock/data.js";
-import renderGoodsList from "./showcase.js";
-import "./assets/style.scss";
+import ApiHandler from "./ApiHandler.js";
+import CartModel from "./CartModel.mjs";
+import ShowcaseModel from "./ShowcaseModel.js";
+import EventEmitter from "./EventEmitter.mjs";
 
+import "./assets/scss/main.scss";
 
-const productList = getProductList(20);
+const API_URL = "/api/v1";
 
-renderGoodsList(productList);
+const api = new ApiHandler(API_URL);
+const eventEmmiter = new EventEmitter();
+
+const cart = new CartModel(api, eventEmmiter);
+const showcase = new ShowcaseModel(api, eventEmmiter, cart);
+
+eventEmmiter.subscribe("showcaseFeched", (data) => {
+  console.log(data);
+});
+
+eventEmmiter.subscribe("cartFeched", (data) => {
+  console.log(data);
+});
+
+showcase.fetch();
+cart.fetch();
